786f0665840d1968d2b5f9a7c249ab11f22b730d
BOluokun <boluokun77@gmail.com>
2022-10-14 18:10:03 +0100

    Make form for home page which takes in the user's text input and displays it on a results page.

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ccb0ce4501150d103e1dae7b8431d6ab70c026903df97cbd'
+
+result_params = {}
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=('GET', 'POST'))
+def book_input():  # put application's code here
+    if request.method == 'POST':
+        title = request.form['title'] or 'Untitled'
+        text = request.form['text']
+
+        if not text:
+            flash('Text is required!')
+        else:
+            result_params['title'] = title
+            result_params['text'] = text
+            return redirect(url_for('result'))
+
+    return render_template('book_input.html')
+
+
+@app.route('/result/')
+def result():
+    return render_template('result.html', result=result_params)
 
 
 if __name__ == '__main__':
