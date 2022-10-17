8f61c598fc3f99d9beb59f8697ac71c2ce460bf2
BOluokun <BOluokun77@gmail.com>
2022-10-17 15:33:42 +0100

    Add a form for user input
    Add a form to allow users to enter a book title and extract.
    
    Fixes: #5.

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("secretkey")
+
+result_params = {}
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=('GET', 'POST'))
+def book_input():
+    if request.method == 'POST':
+        title = request.form['title'] or 'Untitled'
+        text = request.form['text']
+
+        if not text:
+            flash('Text is required!')
+        else:
+            result_params['title'] = title
+            result_params['text'] = text
+            return redirect(url_for('result'))
+
+    return render_template('book_input.html')
+
+
+@app.route('/result/')
+def result():
+    return render_template('result.html', result=result_params)
 
 
 if __name__ == '__main__':
