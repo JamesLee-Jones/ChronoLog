989a3de4d3039bc7d5b3f1ed730ee9312244b877
James Lee-Jones <jl8320@ic.ac.uk>
2022-10-17 18:16:19 +0100

    Address PR comments

--- tests/test_form_data.py
+++ tests/test_form_data.py
@@ -1,17 +1,14 @@
-from app import app
+import json
+import os
+import pytest
 
 
-def test_submit_data():
-    client = app.test_client()
-    response = client.post('/', data={"title": "Test", "text": "This is a test input."}, follow_redirects=True)
-    assert response.status_code == 200
-    assert b"<h1> Results for Test </h1>" in response.data
-    assert b"<p> This is a test input. </p>" in response.data
-    response = client.post('/', data={"title": "Lorem ipsum",
-                                      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
-                                              "eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-                           follow_redirects=True)
-    assert response.status_code == 200
-    assert b"<h1> Results for Lorem ipsum </h1>" in response.data
-    assert b"<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " \
-           b"et dolore magna aliqua. </p>" in response.data
+def test_submit_data(client):
+    directory = 'tests/books'
+
+    for filename in os.listdir(directory):
+        book = json.load(open(os.path.join(directory, filename), "r"))
+        response = client.post('/', data=book, follow_redirects=True)
+        assert response.status_code == 200
+        assert book['title'].encode() in response.data
+        assert book['text'].encode() in response.data
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,7 +1,7 @@
-from app import app
+import pytest
 
 
-def test_submit_data():
-    client = app.test_client()
-    response = client.post('/')
+def test_submit_data(client):
+    response = client.get('/')
+    assert response.status_code == 200
     assert b"<h1>Input a Book! - NLP Character Timeline Generator</h1>"
