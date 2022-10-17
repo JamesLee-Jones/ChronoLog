eb1891d095f1b70bba13add3e4779a0ebe1485b9
James Lee-Jones <jl8320@ic.ac.uk>
2022-10-17 18:21:01 +0100

    Fix test not checking anything

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -4,4 +4,4 @@ import pytest
 def test_submit_data(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"<h1>Input a Book! - NLP Character Timeline Generator</h1>"
+    assert b"Input a Book!" in response.data
