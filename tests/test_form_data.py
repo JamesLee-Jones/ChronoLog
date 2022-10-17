d3179d442c5ce47f13a628b23846da8a1bbfdcec
James Lee-Jones <jl8320@ic.ac.uk>
2022-10-17 18:26:24 +0100

    Remove magic number

--- tests/test_form_data.py
+++ tests/test_form_data.py
@@ -1,6 +1,8 @@
 import json
 import os
+from .constants import OK_RESPONSE_CODE
 import pytest
+
 
 
 def test_submit_data(client):
@@ -9,6 +11,6 @@ def test_submit_data(client):
     for filename in os.listdir(directory):
         book = json.load(open(os.path.join(directory, filename), "r"))
         response = client.post('/', data=book, follow_redirects=True)
-        assert response.status_code == 200
+        assert response.status_code == OK_RESPONSE_CODE
         assert book['title'].encode() in response.data
         assert book['text'].encode() in response.data
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,7 +1,8 @@
+from .constants import OK_RESPONSE_CODE
 import pytest
 
 
 def test_submit_data(client):
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == OK_RESPONSE_CODE
     assert b"Input a Book!" in response.data
