2a7ea9517aa2f9661bad1369a91b8c97fe503ba9
James Lee-Jones <jl8320@ic.ac.uk>
2022-10-18 14:11:56 +0100

    Address PR comment

--- tests/test_form_data.py
+++ tests/test_form_data.py
@@ -3,12 +3,12 @@ import os
 from .constants import OK_RESPONSE_CODE
 import pytest
 
-directory = 'tests/books'
+DIRECTORY = 'tests/books'
 
 
 def test_submit_data(client):
-    for filename in os.listdir(directory):
-        book = json.load(open(os.path.join(directory, filename), "r"))
+    for filename in os.listdir(DIRECTORY):
+        book = json.load(open(os.path.join(DIRECTORY, filename), "r"))
         response = client.post('/', data=book, follow_redirects=True)
         assert response.status_code == OK_RESPONSE_CODE
         assert book['title'].encode() in response.data
