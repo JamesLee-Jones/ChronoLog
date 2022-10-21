653013d8e12bd2fdb2243285d37b78d3498185b7
zeegtab <30591062+zeegtab@users.noreply.github.com>
2022-10-21 11:46:04 +0100

    Renamed JSON validation script and propagated jq return code so job fails is JSON isn't valid.

--- scripts/check_json_formatting.sh
+++ scripts/check_json_formatting.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+return_code=0
 for FILE in `ls ./tests/books`
 do
   jq . "tests/books/$FILE"
   exit_code=$?
     if [ $exit_code -ne 0 ]; then
       echo "$FILE could not be parsed."
+      return_code=1
     fi
 done
+exit $return_code
+
