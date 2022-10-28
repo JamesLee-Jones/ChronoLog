739fdd181451d5ebc5bc9dc499d1e650e7b5de44
dave5202 <david123456@live.co.uk>
2022-10-28 13:22:01 +0100

    Add react app (under the client folder) for the frontend.
    Fixes #44

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect }from 'react'
+
+function App() {
+  const [data, setData] = useState([{}])
+  useEffect(() => {
+    
+  })
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
