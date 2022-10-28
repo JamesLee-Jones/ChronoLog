0142a446654cccf4188111613d1a7418b68b82cd
James Lee-Jones <jl8320@ic.ac.uk>
2022-10-28 12:46:38 +0100

    Changed name of csv file output

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def book_input():
         else:
             session['title'] = title
             session['text'] = nlp.extract_characters(text)
-            nlp.generate_interactions_matrix(text, "test")
+            nlp.generate_interactions_matrix(text, title)
 
             return redirect(url_for('user.result'))
 
