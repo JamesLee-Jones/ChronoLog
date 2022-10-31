049460706c566275cc0973e455069ca17235cfac
dave5202 <david123456@live.co.uk>
2022-10-31 12:00:44 +0000

    Attemp to read csv file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React, {useState, useEffect }from 'react'
+import { readString } from 'react-papaparse'
+import Papa from "papaparse"
 
 function App() {
-  const [data, setData] = useState([{}])
+  file = '../tests/matrices/one_interaction.output.csv';
+  const [parsedCsvData, setParsedCsvData] = useState([]); const parseFile = file => {
+    Papa.parse(file, {
+      header: true,
+      complete: results => {
+        setParsedCsvData(results.data)
+      },
+    });
+  };
+
+  console.log(parsedCsvData);
+
+  FileSystem.readFile()
   useEffect(() => {
     
   })
