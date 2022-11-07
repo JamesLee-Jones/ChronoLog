8f07f38389b139e83d3138428c06c18f717cf8cc
Emily Summerscale <es520@ic.ac.uk>
2022-11-07 12:35:21 +0000

    Commit correct version of code

--- nlp.py
+++ nlp.py
@@ -1,42 +1,29 @@
 import numpy as np
 import spacy
+import json
 
 
-def process_data(text):
-    segments = text.split("\n\n")
+def process_data(text, chapter_regex):
+    """
+    :param text: The full text to be analysed.
+    :param chapter_regex: Regex by which chapters are determined. If chapter_regex=="", split into equal length sections.
+    :return: Returns list of sections
+    """
+    if chapter_regex:
+        timeline = text.split(chapter_regex)
+    else:
+        # TODO: Figure out sensible split that preserves semantics of text i.e. by paragraph
+        timeline = []
     cleaned_data = []
-    for segment in segments:
+    for segment in timeline:
         segment = segment.rstrip()
         segment = segment.replace("\n", " ")
         segment = segment.replace("\r", " ")
         cleaned_data.append(segment)
-
     return cleaned_data
 
 
-def extract_characters(text):
-    try:
-        nlp = spacy.load("en_core_web_md")
-    except OSError:
-        spacy.cli.download("en_core_web_md")
-        nlp = spacy.load("en_core_web_md")
-
-    data = process_data(text)
-    people = {}
-
-    for item in data:
-        doc = nlp(item)
-        for ent in doc.ents:
-            if ent.label_ == "PERSON":
-                if ent.text in people:
-                    people[ent.text] += 1
-                else:
-                    people[ent.text] = 1
-
-    return people
-
-
-def generate_interactions_matrix(text, filename):
+def generate_interactions_matrix(text, prev_matrix, prev_characters):
     try:
         nlp = spacy.load("en_core_web_md")
     except OSError:
@@ -45,10 +32,15 @@ def generate_interactions_matrix(text, filename):
 
     doc = nlp(text)
 
-    characters = list(dict.fromkeys(
-        [ent.text for ent in doc.ents if ent.label_ == "PERSON"]))
+    characters = list(set(prev_characters + [ent.text for ent in doc.ents if ent.label_ == "PERSON"]))
+    characters.sort()
     interactions = {character: {character2: 0 for character2 in characters}
                     for character in characters}
+
+    for i in range(len(prev_characters)):
+        for j in range(i + 1, len(prev_characters)):
+            char1, char2 = prev_characters[i], prev_characters[j]
+            interactions[char1][char2] = prev_matrix[i][j]
 
     for sentence in doc.sents:
         people = list(dict.fromkeys(
@@ -59,19 +51,35 @@ def generate_interactions_matrix(text, filename):
                 interactions[people[j]][people[i]] += 1
 
     interactions_matrix = np.zeros((len(characters), len(characters)))
+    norm_interactions_matrix = np.zeros((len(characters), len(characters)))
 
     for (i, char_interactions) in enumerate(interactions.values()):
         row_sum = sum(char_interactions.values())
         for (j, num_interactions) in enumerate(char_interactions.values()):
-            interactions_matrix[i][j] = num_interactions / \
-                row_sum if row_sum != 0 else 0
+            interactions_matrix[i][j] = num_interactions
+            norm_interactions_matrix[i][j] = num_interactions / row_sum if row_sum != 0 else 0
 
-    character_header = ', '.join(characters)
-    np.savetxt(
-        filename + ".csv",
-        interactions_matrix,
-        delimiter=",",
-        fmt="%.5f",
-        header=character_header)
+    return norm_interactions_matrix, interactions_matrix, characters
 
-    return interactions_matrix
+
+def generate_timeline_json(sections, title):
+    interactions = []
+    characters = []
+    json_contents = {"book": title,
+                     "num_sections": len(sections),
+                     "sections": []
+                     }
+    for section in sections:
+        normalised_interactions, interactions, characters = generate_interactions_matrix(section, interactions,
+                                                                                         characters)
+        json_contents["sections"].append({
+            "names": characters,
+            "matrix": normalised_interactions.tolist()
+        })
+    with open("{}_analysis.json".format(title), "w") as f:
+        f.write(json.dumps(json_contents))
+
+
+sections = ["Harry and Sally went to the park.", "At the park, Harry saw John.", "John then called Margaret to join them."]
+title = "Pulitzer Prize Nomination"
+generate_timeline_json(sections, title)
