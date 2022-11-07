d017ec1f72f7611809c6df11705fb037d9b32bf1
zeegtab <30591062+zeegtab@users.noreply.github.com>
2022-11-07 10:25:32 +0000

    Added graph visualization to React App with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,27 @@
 import React from 'react'
-import Papa from "papaparse"
-import ForceGraph2D from "react-force-graph-2d";
-
+import './App.css';
+import { ForceGraph2D } from 'react-force-graph';
 
 function App() {
-  let url = "https://www.doc.ic.ac.uk/~dee20/web/matrices/two_interactions.output.csv";
-  let results; 
-  Papa.parse(url, {   
-    download: true,
-    complete: function (result) {
-      results = result.data
-    }
-  })
-
   const data = {
     nodes: [
-      { id: results[0][0] },
-      { id: results[0][1] },
-      { id: results[0][2] }
+      { id: "Andrew", name: "Andrew" },
+      { id: "Sarah", name: "Sarah" },
+      { id: "Mark", name: "Mark"}
     ],
     links: [
-      { source: results[0][0], target: results[0][1], value: parseInt(results[1][0]) * 10 },
-      { source: results[0][0], target: results[0][2], value: parseInt(results[2][0]) * 10 },
-      { source: results[0][1], target: results[0][2], value: parseInt(results[2][1]) * 10 }
+      { source: "Andrew", target: "Sarah", value: 1},
+      { source: "Andrew", target: "Mark", value: 0},
+      { source: "Sarah", target: "Mark", value: 0.333},
+
     ]
   };
 
   return (
-    <div>
-
+    <div className="App">
+      <ForceGraph2D graphData={data} />
     </div>
-  )
-  //"start": "react-scripts start",
-    //"build": "react-scripts build",
+  );
 }
 
-export default App
+export default App;
