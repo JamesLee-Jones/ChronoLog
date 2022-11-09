c54b4a1bb8816f36ce5322d889075a07e0ad07e3
BOluokun <boluokun77@gmail.com>
2022-11-09 15:45:36 +0000

    Fix tests.

--- tests/generate_interactions_matrix_test.py
+++ tests/generate_interactions_matrix_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import os
 import nlp
@@ -12,7 +13,15 @@ files = [filename.split('.')[0] for filename in os.listdir(TEXT_DIRECTORY)]
 def test_interactions_matrix(test_name):
     with open(os.path.join(TEXT_DIRECTORY, test_name + '.txt'), 'r') as f:
         text = f.read()
-    nlp.generate_interactions_matrix(text, 'temp')
+    matrix, _, characters = nlp.generate_interactions_matrix(text, 'temp', [])
+    character_header = ', '.join(characters)
+    np.savetxt(
+        os.path.join(CSV_DIRECTORY, test_name + '.output.csv'),
+        matrix,
+        delimiter=",",
+        fmt="%.5f",
+        header=character_header
+    )
     with open('temp.csv', 'r') as a:
         actual = a.read()
     with open(os.path.join(CSV_DIRECTORY, test_name + '.output.csv')) as e:
