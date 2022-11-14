05fb30a8148ac006252f5b0464a98a9018d3ab9f
AbdulRJacob <86776103+AbdulRJacob@users.noreply.github.com>
2022-11-14 10:43:48 +0000

    Improving graph visualisation  (#67)
    * Convert JSON data from output into graph JSON data and display using react force graph
    
    Fixes #56
    
    * Convert multiple section data into graph JSON data for react force graph
    
    * slider that changes graph
    
    * Change link width to now depend on value
    
    * Fix magic number and extra line
    
    Co-authored-by: Abdul <arj220@ic.ac.uk>
    Co-authored-by: dee20 <dee20@ic.ac.uk>
    Co-authored-by: dave5202 <64419812+dave5202@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,113 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import './App.css';
 import { ForceGraph2D } from 'react-force-graph';
+import { Slider } from '@mui/material';
+
+// Converts JSON data from backend into graph JSON data for react force graph
+function convert(data) {
+  let result = [...data["Sections"]]
+  return result.map(convertToGraph)
+
+}
+
+function convertToGraph(data){
+  let nodes = []
+  let links = []
+  let names = data["names"]
+  let matrix = data["matrix"]
+  for (let i = 0; i < names.length; i++) {
+    nodes.push({ id: "id" + String(i), name: names[i] })
+  }
+  for (let j = 0; j < names.length; j++) {
+    for (let k = 0; k < names.length; k++) {
+      if (j !== k && matrix[j][k] !== 0) {
+        links.push({ source: "id" + String(j), target: "id" + String(k), value: matrix[j][k] })
+      }
+    }
+  }
+  const graph = { nodes: nodes, links: links }
+  return graph
+  
+}
 
 function App() {
-  const data = {
-    nodes: [
-      { id: "Andrew", name: "Andrew" },
-      { id: "Sarah", name: "Sarah" },
-      { id: "Mark", name: "Mark"}
-    ],
-    links: [
-      { source: "Andrew", target: "Sarah", value: 1},
-      { source: "Andrew", target: "Mark", value: 0},
-      { source: "Sarah", target: "Mark", value: 0.333},
+  
+  // Mock data needed to allow the convert graph function to run on first pass 
+  let mock_data = {
+    "basic":"Mock ",
+    "numberOfSections":10,
+    "chapterWise": true,
+    "Sections":[
+        {"names":["Bob", "Sam", "C"], 
+    "matrix": [[0, 1, 1], 
+               [1, 0, 1], 
+               [1, 1, 0]]}
+            ]
+    }
 
-    ]
-  };
+  let mock_graph = convert(mock_data)
+  
+  const [data, setData] = useState(mock_graph)
+  const [counter, setCounter] = useState(0)
+
+  // Fetches data outputted by the backend 
+
+  const getData=()=>{
+
+    fetch('timeline.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+    .then(function (response) {
+      return response.json();
+
+    })
+    .then(function (myJson) {
+      let res = convert(myJson)
+      setData(res)
+      });
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+
+  // Rendering Graph 
+
+  const forceRef = useRef(null);
+  let repelStrength = -400;
+
+  useEffect(() => {
+    forceRef.current.d3Force("charge").strength(repelStrength);  });
 
   return (
     <div className="App">
-      <ForceGraph2D graphData={data} />
+       <ForceGraph2D
+          graphData={data[counter]}
+          nodeLabel="name"
+          linkCurvature="curvature"
+          linkWidth="value"
+          linkDirectionalParticleWidth={1}
+          ref={forceRef}
+          centerAt={([500],[500])}
+          />
+      
+        <Slider
+          aria-label="Sections"
+          defaultValue={0}
+          valueLabelDisplay="auto"
+          onChange={(_, value) => {setCounter(value)}}
+          step={1}
+          marks
+          min={0}
+          max={data.length - 1}
+          />
+
     </div>
   );
 }
