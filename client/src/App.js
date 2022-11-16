da31ca6bacdae2fb40c24cee7cb0fd6d16bdf5f9
dave5202 <david123456@live.co.uk>
2022-11-16 17:36:55 +0000

    Add new line

--- client/src/App.js
+++ client/src/App.js
@@ -1,117 +1,130 @@
-import React, { useRef, useEffect, useState } from "react";
-import "./App.css";
-import { ForceGraph2D } from "react-force-graph";
-import { Slider } from "@mui/material";
+import React, { useRef, useEffect, useState } from 'react'
+import './App.css';
+import { ForceGraph2D } from 'react-force-graph';
+import { Slider, Container } from '@mui/material';
 
 // Converts JSON data from backend into graph JSON data for react force graph
 function convert(data) {
-  let result = [...data["Sections"]];
-  return result.map(convertToGraph);
+  let result = [...data["sections"]]
+  return result.map(convertToGraph)
+
 }
 
-function convertToGraph(data) {
-  let nodes = [];
-  let links = [];
-  let names = data["names"];
-  let matrix = data["matrix"];
+function convertToGraph(data){
+  let nodes = []
+  let links = []
+  let names = data["names"]
+  let matrix = data["matrix"]
   for (let i = 0; i < names.length; i++) {
-    nodes.push({ id: "id" + String(i), name: names[i] });
+    nodes.push({ id: "id" + String(i), name: names[i] })
   }
   for (let j = 0; j < names.length; j++) {
     for (let k = 0; k < names.length; k++) {
       if (j !== k && matrix[j][k] !== 0) {
-        links.push({
-          source: "id" + String(j),
-          target: "id" + String(k),
-          value: matrix[j][k],
-        });
+        links.push({ source: "id" + String(j), target: "id" + String(k), value: matrix[j][k] * 10 })
       }
     }
   }
-  const graph = { nodes: nodes, links: links };
-  return graph;
+  const graph = { nodes: nodes, links: links }
+  return graph
+  
 }
 
 function App() {
-  // Mock data needed to allow the convert graph function to run on first pass
+
+  const [displayWidth, setDisplayWidth] = useState(window.innerWidth);
+  const [displayHeight, setDisplayHeight] = useState(window.innerHeight);
+
+  window.addEventListener('resize', () => {
+    setDisplayWidth(window.innerWidth);
+    setDisplayHeight(window.innerHeight);
+  });
+
+  
+  // Mock data needed to allow the convert graph function to run on first pass 
   let mock_data = {
-    basic: "Mock ",
-    numberOfSections: 10,
-    chapterWise: true,
-    Sections: [
-      {
-        names: ["Bob", "Sam", "C"],
-        matrix: [
-          [0, 1, 1],
-          [1, 0, 1],
-          [1, 1, 0],
-        ],
-      },
-    ],
-  };
+    "basic":"Mock ",
+    "numberOfSections":10,
+    "chapterWise": true,
+    "sections":[
+        {"names":["Bob", "Sam", "C"], 
+    "matrix": [[0, 1, 1], 
+               [1, 0, 1], 
+               [1, 1, 0]]}
+            ]
+    }
 
-  let mock_graph = convert(mock_data);
+  let mock_graph = convert(mock_data)
+  
+  const [data, setData] = useState(mock_graph)
+  const [counter, setCounter] = useState(0)
 
-  const [data, setData] = useState(mock_graph);
-  const [counter, setCounter] = useState(0);
+  // Fetches data outputted by the backend 
 
-  // Fetches data outputted by the backend
+  const getData=()=>{
 
-  const getData = () => {
-    fetch("timeline.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
+    fetch('winnie_the_pooh_analysis.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+    .then(function (response) {
+      return response.json();
+
     })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (myJson) {
-        let res = convert(myJson);
-        setData(res);
+    .then(function (myJson) {
+      let res = convert(myJson)
+      setData(res)
       });
-  };
+  }
 
-  useEffect(() => {
-    getData();
-  }, []);
+  useEffect(()=>{
+    getData()
+  },[])
 
-  // Rendering Graph
+
+  // Rendering Graph 
 
   const forceRef = useRef(null);
   let repelStrength = -400;
 
   useEffect(() => {
-    forceRef.current.d3Force("charge").strength(repelStrength);
+    forceRef.current.d3Force("charge").strength(-20);  
+    forceRef.current.d3Force("center").x(-120);  
   });
 
   return (
     <div className="App">
-      <ForceGraph2D
-        graphData={data[counter]}
-        nodeLabel="name"
-        linkCurvature="curvature"
-        linkWidth="value"
-        linkDirectionalParticleWidth={1}
-        ref={forceRef}
-        centerAt={([500], [500])}
-      />
+       <ForceGraph2D
+          graphData={data[counter]}
+          nodeLabel="name"
+          linkCurvature="curvature"
+          linkWidth="value"
+          linkDirectionalParticleWidth={1}
+          width={displayWidth - 300}
+          height={displayHeight - 100}
+          ref={forceRef}
+          centerAt={([500], [500])}
+        />
 
-      <Slider
-        aria-label="Sections"
-        defaultValue={0}
-        valueLabelDisplay="auto"
-        onChange={(_, value) => {
-          setCounter(value);
-        }}
-        step={1}
-        marks
-        min={0}
-        max={data.length - 1}
-      />
+        <Slider
+          aria-label="Sections"
+          defaultValue={0}
+          valueLabelDisplay="auto"
+          onChange={(_, value) => { setCounter(value) }}
+          step={1}
+          marks
+          min={0}
+          max={data.length - 1}
+        />
+      
+
     </div>
   );
 }
 
 export default App;
+
