5f0881a8bcf4629015a512221a8fcfa1947e03de
zeegtab <30591062+zeegtab@users.noreply.github.com>
2022-11-17 13:42:08 +0000

    Resolved issues in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import ChronoLogNavBar from "./ChronoLogNavBar";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import { ForceGraph2D } from "react-force-graph";
 import TimelineNavigaion from "./Slider";
 
@@ -17,103 +17,101 @@ function convertToGraph(data) {
   let names = data["names"];
   let matrix = data["matrix"];
   for (let i = 0; i < names.length; i++) {
-    nodes.push({ id: "id" + String(i), name: names[i] })
+    nodes.push({ id: "id" + String(i), name: names[i] });
   }
   for (let j = 0; j < names.length; j++) {
     for (let k = 0; k < names.length; k++) {
       if (j !== k && matrix[j][k] !== 0) {
-        links.push({ source: "id" + String(j), target: "id" + String(k), value: matrix[j][k] })
+        links.push({
+          source: "id" + String(j),
+          target: "id" + String(k),
+          value: matrix[j][k],
+        });
       }
     }
   }
-  const graph = { nodes: nodes, links: links }
-  return graph
-  
+  const graph = { nodes: nodes, links: links };
+  return graph;
 }
 
 function App() {
-  
-  // Mock data needed to allow the convert graph function to run on first pass 
-  let mock_data = {
-    "basic":"Mock ",
-    "numberOfSections":10,
-    "chapterWise": true,
-    "Sections":[
-        {"names":["Bob", "Sam", "C"], 
-    "matrix": [[0, 1, 1], 
-               [1, 0, 1], 
-               [1, 1, 0]]}
-            ]
-    }
+  const [displayWidth, setDisplayWidth] = useState(window.innerWidth);
+  const [displayHeight, setDisplayHeight] = useState(window.innerHeight);
 
-  let mock_graph = convert(mock_data)
-  
-  const [data, setData] = useState(mock_graph)
-  const [counter, setCounter] = useState(0)
+  window.addEventListener("resize", () => {
+    setDisplayWidth(window.innerWidth);
+    setDisplayHeight(window.innerHeight);
+  });
 
-  // Fetches data outputted by the backend 
+  const [data, setData] = useState({ nodes: {}, links: {} });
+  const [counter, setCounter] = useState(0);
 
-  const getData=()=>{
+  // Fetches data outputted by the backend
 
-    fetch('timeline.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-    .then(function (response) {
-      return response.json();
-
+  const getData = () => {
+    fetch("timeline.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
-    .then(function (myJson) {
-      let res = convert(myJson)
-      setData(res)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        let res = convert(myJson);
+        setData(res);
       });
-  }
-
-  useEffect(()=>{
-    getData()
-  },[])
-
-
-  // Rendering Graph 
-
-  const forceRef = useRef(null);
-  let repelStrength = -400;
+  };
 
   useEffect(() => {
-    forceRef.current.d3Force("charge").strength(repelStrength);  });
+    getData();
+  }, []);
+
+  // Rendering Graph
+
+  const forceRef = useRef(null);
+  let repelStrength = -10;
+  let centering = -120;
+  let zoomingTime = 50;
+  let padding = 130;
+  let widthCentering = 300;
+  let heightCentering = 100;
+
+  useEffect(() => {
+    forceRef.current.d3Force("charge").strength(repelStrength);
+    forceRef.current.d3Force("center").x(centering);
+    forceRef.current.zoomToFit(zoomingTime, padding);
+  });
 
   return (
     <>
-    <ChronoLogNavBar />
-    <div class="chronolog-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <img src="../ChronoLogo.png" class="img-fluid" alt="Responsive image" />
-    <div class="about-chronolog">
-      <p>Beautiful data generation and visualization of .</p>
-    </div>
-    </div>
-    <div className="App">
-      <ForceGraph2D
-        graphData={data[counter]}
-        nodeLabel="name"
-        linkCurvature="curvature"
-        linkWidth="value"
-        linkDirectionalParticleWidth={1}
-        width={displayWidth - widthCentering}
-        height={displayHeight - heightCentering}
-        ref={forceRef}
-        nodeAutoColorBy={"name"}
-      />
+      <ChronoLogNavBar />
+      <div class="chronolog-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <img src="../ChronoLogo.png" class="img-fluid" alt="Responsive image" />
+        <div class="about-chronolog">
+          <p>Beautiful data generation and visualization of .</p>
+        </div>
+      </div>
+      <div className="App">
+        <ForceGraph2D
+          graphData={data[counter]}
+          nodeLabel="name"
+          linkCurvature="curvature"
+          linkWidth="value"
+          linkDirectionalParticleWidth={1}
+          width={displayWidth - widthCentering}
+          height={displayHeight - heightCentering}
+          ref={forceRef}
+          nodeAutoColorBy={"name"}
+        />
 
-      <TimelineNavigaion
-        maxval={data.length - 1}
-        setCounter={setCounter}
-        TimelineNavigaion
-      />
-    </div>
+        <TimelineNavigaion
+          maxval={data.length - 1}
+          setCounter={setCounter}
+          TimelineNavigaion
+        />
+      </div>
     </>
   );
 }
--- client/src/ChronoLogNavBar.js
+++ client/src/ChronoLogNavBar.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import './Navbar.css'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./Navbar.css";
 
 function ChronoLogNavBar() {
   return (
     <Navbar className="color-nav" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand className="nav-brand" href="#home">ChronoLog.</Navbar.Brand>
+        <Navbar.Brand className="nav-brand" href="#home">
+          ChronoLog.
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className='nav-link' href="#home">About</Nav.Link>
+            <Nav.Link className="nav-link" href="#home">
+              About
+            </Nav.Link>
             <NavDropdown title="Library" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/2.1">Winne The Pooh</NavDropdown.Item>
-              <NavDropdown.Item href="#action/2.2">Little Women</NavDropdown.Item>
-              <NavDropdown.Item href="#action/2.3">Harry Potter and the Philosopher's Stone</NavDropdown.Item>
+              <NavDropdown.Item href="#action/2.1">
+                Winne The Pooh
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/2.2">
+                Little Women
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/2.3">
+                Harry Potter and the Philosopher's Stone
+              </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Developers" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">GitHub</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Documentation</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Documentation
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
