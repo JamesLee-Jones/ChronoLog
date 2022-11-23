edebbb510cc8e5a75f0178fab189c5fea41ce378
Emily Summerscale <40636647+ESummerscale@users.noreply.github.com>
2022-11-23 17:16:08 +0000

    Process more books and add metadata
    Co-authored-by: BOluokun <boluokun77@gmail.com>

--- backend/nlp.py
+++ backend/nlp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import spacy
+import regex as re
 import json
 
 JSON_DIRECTORY = "timelines/"
@@ -8,7 +9,7 @@ JSON_DIRECTORY = "timelines/"
 def process_data(text, chapter_regex, num_splits, quiet):
     """
     :param text: The full text to be analysed.
-    :param chapter_regex: Regex by which chapters are determined. If chapter_regex=="", split into equal length sections.
+    :param chapter_regex: Regex by which chapters are determined. If chapter_regex="", split into equal length sections.
     :param num_splits: The number of sections to split the text into if regex is not given
     :param quiet: Flag to silence print statements
     :return: Returns list of sections
@@ -17,11 +18,8 @@ def process_data(text, chapter_regex, num_splits, quiet):
     if chapter_regex:
         if not quiet:
             print("Splitting book into chapters...")
-        timeline = list(
-            filter(
-                lambda x: not (
-                    x.strip() is None),
-                chapter_regex.split(text)))
+        timeline = list(chapter_regex.split(text))
+        timeline.pop(0)
     else:
         # Splits by paragraph, then joins paragraphs back up into NUM_SPLITS
         # sections
@@ -35,6 +33,7 @@ def process_data(text, chapter_regex, num_splits, quiet):
         timeline = ["\n".join(paragraphs[i * k + min(i,
                                                      m): (i + 1) * k + min(i + 1,
                                                                            m)]) for i in range(num_sections)]
+        filter(lambda ps: ("".join(ps).strip() != ""), timeline)
     cleaned_data = []
     if not quiet:
         print("Cleaning text...")
@@ -49,8 +48,7 @@ def process_data(text, chapter_regex, num_splits, quiet):
 
 
 def pool_characters(all_characters: list[str], character_dict: dict[str, str]) -> list[str]:
-    character_matches = {ch: [ch2 for ch2 in all_characters if ch in ch2.split(' ')] for ch in all_characters}
-    # print(character_matches)
+    character_matches = {ch: [ch2 for ch2 in all_characters if ch in re.split(" |-", ch2)] for ch in all_characters}
     full_names = set()
     for name in character_matches:
         full_name = str(max(character_matches[name], key=len)) if character_matches[name] else name
@@ -60,7 +58,26 @@ def pool_characters(all_characters: list[str], character_dict: dict[str, str]) -
     return list(full_names)
 
 
-def generate_interactions_matrix(text, prev_matrix, prev_characters, character_dict):
+def get_characters(doc, prev_characters, character_dict):
+    all_characters = sorted(
+        set(prev_characters +
+            [ent.text.title().replace("_", "").removesuffix("'S") for ent in doc.ents if ent.label_ == "PERSON"]))
+    return sorted(pool_characters(all_characters, character_dict))
+
+
+def setup_interactions(characters, prev_characters, character_dict, prev_matrix):
+    interactions = {character: {character2: 0.0 for character2 in characters}
+                    for character in characters}
+    for i in range(len(prev_characters)):
+        for j in range(i + 1, len(prev_characters)):
+            char1, char2 = prev_characters[i], prev_characters[j]
+            interactions[character_dict[char1]][character_dict[char2]] = prev_matrix[i][j]
+            interactions[character_dict[char2]][character_dict[char1]] = prev_matrix[j][i]
+    return interactions
+
+
+def generate_interactions_matrix(text, prev_matrix, prev_characters, character_dict, first_interactions_overall={},
+                                 first_interactions_per_char={}):
     try:
         nlp = spacy.load("en_core_web_lg")
     except OSError:
@@ -68,33 +85,43 @@ def generate_interactions_matrix(text, prev_matrix, prev_characters, character_d
         nlp = spacy.load("en_core_web_lg")
 
     doc = nlp(text)
-
-    all_characters = sorted(
-        set(prev_characters + [ent.text for ent in doc.ents if ent.label_ == "PERSON"]))
-    characters = sorted(pool_characters(all_characters, character_dict))
-    interactions = {character: {character2: 0.0 for character2 in characters}
-                    for character in characters}
-
-    for i in range(len(prev_characters)):
-        for j in range(i + 1, len(prev_characters)):
-            char1, char2 = prev_characters[i], prev_characters[j]
-            interactions[character_dict[char1]][character_dict[char2]] = prev_matrix[i][j]
-            interactions[character_dict[char2]][character_dict[char1]] = prev_matrix[j][i]
+    characters = get_characters(doc, prev_characters, character_dict)
+    interactions = setup_interactions(characters, prev_characters, character_dict, prev_matrix)
 
     for sentence in doc.sents:
         people = list(dict.fromkeys(
-            [ent.text for ent in sentence.ents if ent.label_ == "PERSON"]))
+            [ent.text.title().replace("_", "").removesuffix("'S") for ent in sentence.ents if ent.label_ == "PERSON"]))
         for i in range(len(people)):
             for j in range(i + 1, len(people)):
-                interactions[character_dict[people[i]]][character_dict[people[j]]] += 1
-                interactions[character_dict[people[j]]][character_dict[people[i]]] += 1
+                # Track first interactions
+                first_char = min(character_dict[people[i]], character_dict[people[j]])
+                second_char = max(character_dict[people[i]], character_dict[people[j]])
+                if first_char == second_char:
+                    continue
+                update_interactions_records(first_interactions_per_char, first_interactions_overall, interactions,
+                                            sentence.text, first_char, second_char)
+                # Increment interactions
+                interactions[first_char][second_char] += 1
+                interactions[second_char][first_char] += 1
+
     interactions_matrix = np.zeros((len(characters), len(characters)))
 
     for (i, char_interactions) in enumerate(interactions.values()):
         for (j, num_interactions) in enumerate(char_interactions.values()):
             interactions_matrix[i][j] = num_interactions
-
     return interactions_matrix, characters
+
+
+def update_interactions_records(first_interactions_per_char, first_interactions_overall, interactions, sentence,
+                                first_char, second_char):
+    if first_interactions_per_char.get(first_char) is None:
+        first_interactions_per_char[first_char] = {}
+    if first_interactions_per_char.get(first_char).get(second_char) is None:
+        first_interactions_per_char[first_char].update({second_char: sentence})
+    if not sum(interactions[first_char].values()):
+        first_interactions_overall[first_char] = {"with": second_char, "context": sentence}
+    if not sum(interactions[second_char].values()):
+        first_interactions_overall[second_char] = {"with": first_char, "context": sentence}
 
 
 def normalise_matrix(matrix):
@@ -110,7 +137,7 @@ def calculate_threshold(values, percentile):
     return np.percentile(values, percentile)
 
 
-def prune_matrices(matrices, characters_timeline, quiet, percentile):
+def prune(matrices, characters_timeline, quiet, percentile, interactions_overall, interactions_per_character):
     if not quiet:
         print("Pruning timeline matrices...")
     characters_interactions = {
@@ -121,13 +148,34 @@ def prune_matrices(matrices, characters_timeline, quiet, percentile):
         print("Threshold: ", threshold)
     unimportant_characters = [ch for (ch, x) in characters_interactions.items() if x < threshold]
 
+    matrices, characters_timeline = prune_matrices(matrices, characters_timeline, unimportant_characters)
+    interactions_overall, interactions_per_character = prune_metadata(unimportant_characters, interactions_overall,
+                                                                      interactions_per_character)
+
+    return matrices, characters_timeline, interactions_overall, interactions_per_character
+
+
+def prune_matrices(matrices, characters_timeline, unimportant_characters):
     for i in range(len(matrices)):
         for character in [ch for ch in characters_timeline[i] if ch in unimportant_characters]:
             j = characters_timeline[i].index(character)
             matrices[i] = np.delete(np.delete(matrices[i], j, axis=0), j, axis=1)
             characters_timeline[i].pop(j)
-
     return matrices, characters_timeline
+
+
+def prune_metadata(unimportant_characters, interactions_overall, interactions_per_character):
+    for c in unimportant_characters:
+        if c in interactions_overall:
+            del interactions_overall[c]
+        if c in interactions_per_character:
+            del interactions_per_character[c]
+        for key in list(interactions_per_character.keys()):
+            if c in interactions_per_character[key]:
+                del interactions_per_character[key][c]
+                if not interactions_per_character[key]:
+                    del interactions_per_character[key]
+    return interactions_overall, interactions_per_character
 
 
 def generate_timeline_json(sections, title, quiet, unpruned, percentile):
@@ -141,21 +189,27 @@ def generate_timeline_json(sections, title, quiet, unpruned, percentile):
 
     unnormalised_matrices = []
     character_lists = []
+    first_interactions_overall = {}
+    first_interactions_per_char = {}
     for (i, section) in enumerate(sections):
         if not quiet:
             print("Analysing section {} of {}...".format(i + 1, len(sections)))
         interactions, characters = generate_interactions_matrix(
-            section, interactions, characters, {})
+            section, interactions, characters, {}, first_interactions_overall, first_interactions_per_char)
         unnormalised_matrices.append(interactions)
         character_lists.append(characters)
     if not unpruned:
-        unnormalised_matrices, character_lists = prune_matrices(unnormalised_matrices, character_lists, quiet, percentile)
+        unnormalised_matrices, character_lists, first_interactions_overall, first_interactions_per_char = \
+            prune(unnormalised_matrices, character_lists, quiet, percentile, first_interactions_overall,
+                  first_interactions_per_char)
     normalised_matrices = list(map(normalise_matrix, unnormalised_matrices))
     for i in range(len(character_lists)):
         json_contents["sections"].append({
             "names": character_lists[i],
             "matrix": normalised_matrices[i].tolist()
         })
+    json_contents["first_interactions_between_characters"] = first_interactions_per_char
+    json_contents["first_interactions_overall"] = first_interactions_overall
     with open(file_path, "w", newline='\r\n') as f:
         f.write(json.dumps(json_contents, indent=2))
     if not quiet:
