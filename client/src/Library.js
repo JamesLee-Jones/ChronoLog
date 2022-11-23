a0b898324aa3f25915e2eae9949313218ad77c22
zeegtab <30591062+zeegtab@users.noreply.github.com>
2022-11-23 17:52:40 +0000

    Fetched book covers using Gutendex's web API

--- client/src/Library.js
+++ client/src/Library.js
@@ -1,11 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import "./App.css";
 import "./Library.css";
 
+function convert(data) {
+  let bookName = data["results"][0]["title"]
+  let bookAuthor = data["results"][0]["authors"][0]["name"]
+  let bookCoverUrl = data["results"][0]["formats"]["image/jpeg"]
+  console.log(bookCoverUrl);
+  return {id : 1, title : bookName, authors : bookAuthor, cover: bookCoverUrl};
+}
+
 function Library() {
+
+  const [data, setData] = useState({ id: {}, title: {}, authors: {}, cover: {}});
+
+  const getData = () => {
+    fetch("https://gutendex.com/books?search=little women", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        let res = convert(myJson)
+        setData(res);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+    console.log(data)
+  }, []);
+
   useEffect(() => {
     document.body.style.backgroundColor = "#eae0d5";
   });
@@ -19,7 +51,7 @@ function Library() {
         {files.map((filename) => (
           <Col>
             <Card className="text-center">
-              <Card.Img variant="top" src="../ChronoLogoMini.png" />
+              <Card.Img variant="top" src={data.cover} />
               <Card.Body>
                 <Card.Title>{filename.replaceAll("_", " ")}</Card.Title>
                 <a href={filename} class="btn stretched-link">
