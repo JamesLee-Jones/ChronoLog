e8659abfd013f3ab119386dbf385fa974dac32c4
BOluokun <BOluokun77@gmail.com>
2022-11-23 18:33:21 +0000

    Deploying to gh-pages from @ JamesLee-Jones/ChronoLog@9246e1f72e5d7585d727eb4a544788a3e6ea64f7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfdab7cb8c0a423af28b9467f72822db.js"
+  "/precache-manifest.582c570386ad00ad24e304cd078d0580.js"
 );
 
 workbox.clientsClaim();
