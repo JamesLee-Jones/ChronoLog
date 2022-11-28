b8d7840d0bac176cf811efa670379c5ac00dcf18
BOluokun <boluokun77@gmail.com>
2022-11-28 23:27:28 +0000

    Updated the tests

--- tests/generate_interactions_matrix_test.py
+++ tests/generate_interactions_matrix_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 import os
-from backend import nlp
+from backend import nlp, character_interactions_processor as cip
 
 TEXT_DIRECTORY = 'tests/books/'
 CSV_DIRECTORY = 'tests/matrices/'
@@ -13,7 +13,7 @@ files = [filename.split('.')[0] for filename in os.listdir(TEXT_DIRECTORY)]
 def test_interactions_matrix(test_name):
     with open(os.path.join(TEXT_DIRECTORY, test_name + '.txt'), 'r') as f:
         text = f.read()
-    actual = nlp.normalise_matrix(nlp.generate_interactions_matrix(text, [], [], {})[0])
+    actual = cip.normalise_matrix(nlp.InteractionsCounter().generate_interactions_matrix(text)[0])
     expected = np.loadtxt(
         os.path.join(
             CSV_DIRECTORY,
