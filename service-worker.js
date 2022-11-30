7df8e2817094cf2f89d7c5edf477de7e5904bd9b
ESummerscale <40636647+ESummerscale@users.noreply.github.com>
2022-11-30 17:38:46 +0000

    Deploying to gh-pages from @ JamesLee-Jones/ChronoLog@5ebfc7497f5a65770578ec9c7eded5a4e7f7d7bd 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a24f8c2af145f478604781aa81d2eea.js"
+  "/precache-manifest.58cb69ee4caf1d3f1d67becf1f8a9ae3.js"
 );
 
 workbox.clientsClaim();
