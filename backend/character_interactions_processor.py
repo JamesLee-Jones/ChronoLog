93cf2b268196ba40353d50c933f3f7fe47628298
dee20 <dee20@ic.ac.uk>
2022-11-30 16:49:52 +0000

    Lint nlp.py

--- backend/character_interactions_processor.py
+++ backend/character_interactions_processor.py
@@ -121,23 +121,26 @@ class CharacterInteractionsProcessor:
         return matrix
 
     @staticmethod
-    def network_analysis(matrix: np.ndarray, character_list: list):
+    def network_analysis(matrix, character_list):
         scale_factor = 10
-        G = nx.from_numpy_matrix(matrix * 10)
         # Create a list of graphs
-        characters_to_nodes = {}
+        G = nx.Graph()
         for i in range(len(character_list)):
-            characters_to_nodes[i] = character_list[i]
-        graph = nx.relabel_nodes(G, characters_to_nodes)
+            G.add_node(character_list[i])
+        for j in range(len(character_list) - 1):
+            for k in range(j, len(character_list)):
+                G.add_edge(character_list[j], character_list[k], weight=(matrix[i][j] + matrix[j][k]) * scale_factor)
+
+        graph = G
         avg_node_connectivity = []
         avg_clustering = []
 
         # The most important character and how many characters they are connected to
         centrality = nx.degree_centrality(graph)
         centrality_values = centrality.values()
-        degrees = sorted([(d, n) for n, d in graph.degree()])
+        degrees = sorted([(d, n) for n, d in graph.degree(weight="weight")])
         most_important_node = degrees[-1][1]
-        degree_of_node = degrees[-1][0]
+        degree_of_node = graph.degree(most_important_node)
         centrality_of_node = centrality[most_important_node]
         avg_centrality = 0 if len(centrality_values) == 0 else sum(centrality_values) / len(centrality_values)
 
@@ -149,14 +152,12 @@ class CharacterInteractionsProcessor:
                 avg_clustering.append(nx.average_clustering(C, weight="weight"))
         centrality = nx.degree_centrality(graph)
 
-        return 0 if len(avg_node_connectivity) == 0 else sum(avg_node_connectivity) / len(avg_node_connectivity), \
-               0 if len(avg_clustering) == 0 else sum(avg_clustering) / len(
-                   avg_clustering), nx.number_connected_components(graph), (
-               most_important_node, degree_of_node, centrality_of_node), avg_centrality
+        return 0 if len(avg_node_connectivity) == 0 else sum(avg_node_connectivity) / len(avg_node_connectivity), 0 \
+            if len(avg_clustering) == 0 else sum(avg_clustering) / len(avg_clustering), nx.number_connected_components(
+            graph), (most_important_node, degree_of_node, centrality_of_node), avg_centrality
         # Add all of the nodes from the characters as a mapping {i: characters[i]}
         # For matrx[i][j] add an edge (character_list[i], character_list[j, {'weight': matrix[i][j]]))
         # For each graph apply the networkX functions and return them as apart of the section
-
     def generate_timeline_json(self, title: str):
         file_path = JSON_DIRECTORY + "{}_analysis.json".format(title.replace(' ', '_'))
         json_contents = {"book": title.replace('_', ' ').title(),
