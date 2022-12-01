d1de6c19cfb5d33ddaa883ace283f8467dd5a34b
JamesLee-Jones <36175797+JamesLee-Jones@users.noreply.github.com>
2022-12-01 13:28:26 +0000

    Deploying to gh-pages from @ JamesLee-Jones/ChronoLog@d0d3038b53fb5bc7256bb0de764f153bdd670f52 🚀

---
+++ precache-manifest.23c5905b8ffc050a8b067e81678a4213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "329e47184e8e2e2ef2dd",
+    "url": "/static/css/main.a63eda96.chunk.css"
+  },
+  {
+    "revision": "329e47184e8e2e2ef2dd",
+    "url": "/static/js/main.fe230605.chunk.js"
+  },
+  {
+    "revision": "48c9573e7150ceeffda6",
+    "url": "/static/js/runtime~main.c6162133.js"
+  },
+  {
+    "revision": "b455717e2e4f670a21df",
+    "url": "/static/css/2.d3bb1ed2.chunk.css"
+  },
+  {
+    "revision": "b455717e2e4f670a21df",
+    "url": "/static/js/2.5089cfa4.chunk.js"
+  },
+  {
+    "revision": "dcd66d9b6e980b86cf48",
+    "url": "/static/js/3.86c2124f.chunk.js"
+  },
+  {
+    "revision": "06d0638915109a9ed5161fd610e06829",
+    "url": "/index.html"
+  }
+];
