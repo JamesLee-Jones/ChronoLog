0e2db2a9178f4c65b47d4a35552ae47f1b2da46e
Zarya <zeegtab@Zaryas-MBP-2.lan>
2023-01-06 14:26:25 +0000

    Marked resolutions in Graphs.js

--- client/src/NewHomepage.js
+++ client/src/NewHomepage.js
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import "./App.css";
-import {Button, IconButton, SvgIcon} from "@mui/material";
-import {Circle, KeyboardArrowLeft, KeyboardArrowRight} from "@mui/icons-material";
+import {Button, IconButton} from "@mui/material";
+import {KeyboardArrowLeft, KeyboardArrowRight} from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 import CircleIcon from '@mui/icons-material/Circle';
+import {ForceGraph2D} from "react-force-graph";
+import * as d3 from "d3";
 
 // function HomePage() {
 //   const [active1, setActive1] = useState(false);
@@ -144,19 +146,77 @@ function HomePage() {
         setSelected((row_id + 1) % 5);
     }
 
+    useEffect(() => {
+        document.body.style.backgroundColor = "#F5EEE2";
+    });
+
+    let node = [
+        {id: "id1", name: "Harry", val: "3"},
+        {id: "id2", name: "Ron", val: "3"},
+        {id: "id3", name: "Hermione", val: "3"},
+        {id: "id4", name: "Dumbledore", val: "3"},
+        {id: "id5", name: "Snape", val: "3"},
+        {id: "id6", name: "Dobby", val: "3"},
+        {id: "id7", name: "Draco", val: "3"},
+        {id: "id8", name: "Neville", val: "3"},
+
+    ];
+
+    let link = [
+        {source: "id1", target: "id4"},
+        {source: "id2", target: "id3"},
+        {source: "id2", target: "id4"},
+        {source: "id3", target: "id4"},
+        {source: "id4", target: "id5"},
+        {source: "id4", target: "id6"},
+        {source: "id4", target: "id7"},
+        {source: "id5", target: "id8"},
+    ];
+
+    const width = 1000;
+    const height = 300;
+
+    const forceRef = useRef();
+
+    useEffect(() => {
+        forceRef.current.d3Force("charge", d3.forceManyBody().strength(-100));
+        forceRef.current.d3Force("center", d3.forceCenter(0, 0));
+        forceRef.current.d3Force("collide", d3.forceCollide());
+        forceRef.current.d3Force("y", d3.forceY(-10));
+        forceRef.current.d3Force("x", d3.forceX(-10));
+    }, []);
+
     return (
         <>
-            <div className="chronolog-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <img src="../ChronoLogo.png" className="img-fluid" alt="ChronoLogo"/>
+            <main className="container" id="chronolog-header" role="main">
+                <div className="row justify-content-start">
+                    <div className="col-4">
+                        <img src="../ChronoLogoTransparent.png" className="img-fluid" alt="ChronoLogo"/>
+                    </div>
+                    <div className="col-8 homeGraph">
+                        <ForceGraph2D
+                            graphData={{nodes: node, links: link}}
+                            nodeLabel="name"
+                            linkWidth={4}
+                            linkDirectionalParticleWidth={4}
+                            nodeAutoColorBy={"name"}
+                            width={width}
+                            height={height}
+                            ref={forceRef}
+                            enablePanInteraction={true}
+                            enableZoomInteraction={true}
+                        />
+                    </div>
+                </div>
                 <div className="about-chronolog">
                     <p>Beautiful data.</p>
                 </div>
-            </div>
+            </main>
             <div className="container">
                 <div className="row text-center">
                     <div className="col-sm text-center">
-                        <Button style={{backgroundColor:"#22333b" }}>
-                            <KeyboardArrowLeft style={{color: "#eae0d5" }} onClick={() => selectLeft(selected)}/>
+                        <Button style={{backgroundColor: "#22333b"}}>
+                            <KeyboardArrowLeft style={{color: "#eae0d5"}} onClick={() => selectLeft(selected)}/>
                         </Button>
                     </div>
                     {buttons.map((list) => (
@@ -173,19 +233,21 @@ function HomePage() {
                             {/*    {list.title}*/}
                             {/*</Button>*/}
                             <IconButton>
-                            <CircleIcon variant="outlined"
-                            id={list.id}
-                            onClick={() => handleColor(list)}
-                            style={{color: list.id === selected ? "#22333b" : "#eae0d5",
-                            borderColor: list.id === selected ? "#22333b" : "#eae0d5"}}>
-                            </CircleIcon>
+                                <CircleIcon variant="outlined"
+                                            id={list.id}
+                                            onClick={() => handleColor(list)}
+                                            style={{
+                                                color: list.id === selected ? "#22333b" : "#eae0d5",
+                                                borderColor: list.id === selected ? "#22333b" : "#eae0d5"
+                                            }}>
+                                </CircleIcon>
                             </IconButton>
                         </div>
 
                     ))}
                     <div className="col-sm text-center">
-                        <Button style={{backgroundColor:"#22333b" }}>
-                            <KeyboardArrowRight style={{color: "#eae0d5" }} onClick={() => selectRight(selected)}/>
+                        <Button style={{backgroundColor: "#22333b"}}>
+                            <KeyboardArrowRight style={{color: "#eae0d5"}} onClick={() => selectRight(selected)}/>
                         </Button>
                     </div>
                 </div>
